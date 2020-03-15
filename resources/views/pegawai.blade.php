779234721a4814f58b28e5148d97309e54cc2db6
adrianpaskalis <adrian.paskalis@si.ukdw.ac.id>
2020-03-15 20:51:28 +0700

    eloquent laravel

---
+++ resources/views/pegawai.blade.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" rel="stylesheet">
+        <title>Tutorial Laravel #21 : CRUD Eloquent Laravel </title>
+    </head>
+    <body>
+        <div class="container">
+            <div class="card mt-5">
+                <div class="card-header text-center">
+                    CRUD Data Pegawai 
+                </div>
+                <div class="card-body">
+                    <a href="/pegawai/tambah" class="btn btn-primary">Input Pegawai Baru</a>
+                    <br/>
+                    <br/>
+                    <table class="table table-bordered table-hover table-striped">
+                        <thead>
+                            <tr>
+                                <th>Nama</th>
+                                <th>Alamat</th>
+                                <th>OPSI</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($pegawai as $p)
+                            <tr>
+                                <td>{{ $p->nama }}</td>
+                                <td>{{ $p->alamat }}</td>
+                                <td>
+                                    <a href="/pegawai/edit/{{ $p->id }}" class="btn btn-warning">Edit</a>
+                                    <a href="/pegawai/hapus/{{ $p->id }}" class="btn btn-danger">Hapus</a>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
